98360407ee376df7ce0b48aec026ada5be1168b4
Ibragimov-96 <dima.ibragimov.1996@ukr.net>
2023-03-30 09:58:47 +0300

    mongoose

--- models/contacts.js
+++ models/contacts.js
@@ -1,51 +1,53 @@
 
-const fs = require('fs/promises')
-const path = require("path");
-const contactsPath = path.resolve("models", "contacts.json")
 
-const listContacts = async () => {
- const list = await fs.readFile(contactsPath, 'utf-8')
+const {Contact}= require('../db/contactModel.js');
 
- return JSON.parse(list)
+
+const listContacts = async (req, res) => {
+  const list = await Contact.find({})
+
+return list
 }
 
 const getContactById = async (contactId) => {
-  const list = await fs.readFile(contactsPath, 'utf-8')
-  const findContactId = JSON.parse(list).find(contact => contact.id.toString() === contactId)
-  console.log(findContactId)
-  return findContactId
+  const list = await Contact.findById(contactId)
+
+  return list
   
 }
 
 const removeContact = async (contactId) => {
-  const list = await fs.readFile(contactsPath, 'utf-8')
-  const deleteContact = JSON.parse(list).filter(contact => contact.id.toString() !== contactId)
-  await fs.writeFile(contactsPath, JSON.stringify(deleteContact))
-  return deleteContact
+  const list = await Contact.findByIdAndRemove(contactId)
+  return list
 }
 
 const addContact = async (name, email, phone) => {
-  const list = await fs.readFile(contactsPath, 'utf-8')
-  const addContact = [...JSON.parse(list), {id:name, email, phone,name}]
-await fs.writeFile(contactsPath, JSON.stringify(addContact))
-return addContact}
+
+  const list = new Contact ({name, email, phone})
+ 
+await list.save()
+}
 
 
 const updateContact = async (contactId, body) => {
-  const list = JSON.parse(await fs.readFile(contactsPath, 'utf-8'))
-  const index = list.findIndex(obj => obj.id.toString()===contactId);
-  if(index === -1){
-    return null
-  }
-  list[index]={...list[index], ...body}
-  await fs.writeFile(contactsPath, JSON.stringify(list))
-return list
+  const {name,email,phone}= body
+  const list = await Contact.findByIdAndUpdate(contactId,{$set:{name,email,phone}})
+  await list.save()
+
 }
 
+
+const updateStatusContact = async (contactId,body)=>{
+  const{favorite}=body
+ 
+  const list = await Contact.findByIdAndUpdate(contactId,{$set:{favorite}})
+  await list.save()
+}
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
+  updateStatusContact
 }
